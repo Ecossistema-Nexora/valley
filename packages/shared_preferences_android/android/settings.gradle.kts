c98c2732ee7a455473148b7909f7a101770c1a11
interflownex <nazareteandersoncarvalho@gmail.com>
2026-04-23 03:16:19 -0300

    feat: implement shared_preferences_android plugin for Flutter

---
+++ packages/shared_preferences_android/android/settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.library") version "8.13.1"
+        id("org.jetbrains.kotlin.android") version "2.3.0"
+    }
+}
+
+rootProject.name = "shared_preferences_android"
