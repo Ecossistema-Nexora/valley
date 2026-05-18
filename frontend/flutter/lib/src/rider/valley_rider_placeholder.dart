d220e542615bbf6613b0fadf5e90f1c8b7ef0698
interflownex <interflow.br@gmail.com>
2026-05-18 18:29:48 -0300

    feat: add Valley Rider placeholder

---
+++ frontend/flutter/lib/src/rider/valley_rider_placeholder.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ValleyRiderPlaceholder extends StatelessWidget {
+  const ValleyRiderPlaceholder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Valley Rider'),
+      ),
+    );
+  }
+}
